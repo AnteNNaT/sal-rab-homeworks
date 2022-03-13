0fa353f694b68cb696e1d5020458201c3271a490
Anna Talankina <anuta_t@mail.ru>
2022-03-14 01:53:01 +0500

    Task 2.1 done

--- task_2/solution.js
+++ task_2/solution.js
@@ -5,17 +5,13 @@ function calcShipping(sum, min, shipping) {
 
     // Задание №2.1. Рассчитать доставку
 
-    // создайте переменную shippingSum
-
-    // если productsSum равно 0,
-    // то shippingSum присвоить значение 0
-
-    // если productsSum Больше или равна freeShippingMinSum,
-    // то shippingSum присвоить значение 0
-
-    // если productsSum больше 0 и меньше freeShippingMinSum,
-    // то shippingSum присвоить значение shippingPrice
-
+    let shippingSum;
+    
+    if (productsSum > 0 && productsSum < freeShippingMinSum){
+        shippingSum = shippingPrice;
+    } else {
+        shippingSum = 0;
+    }
     // Конец решения задания №2.1.
 
     return shippingSum;
